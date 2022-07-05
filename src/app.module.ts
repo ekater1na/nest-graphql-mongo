51dae7ecc2c79ff08f66f5c10b9f990272ba6079
ekater1na <ekaterina_l@tut.by>
2022-07-05 19:26:08 +0300

    build: remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { CatsModule } from './cats/cats.module';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: 'src/schema.gql',
     }),
-    CatsModule,
-    MongooseModule.forRoot('mongodb://localhost/nest'),
   ],
   controllers: [AppController],
   providers: [AppService],
