caa45fb1eb7a71073e373f972d8911fc2f4ea36b
ekater1na <ekaterina_l@tut.by>
2022-07-05 19:23:41 +0300

    feat: use code-first approach

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { CatsModule } from './cats/cats.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/schema.gql',
+    }),
+    CatsModule,
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
